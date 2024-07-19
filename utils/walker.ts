7d66d34071d7d31b86fb27df271f38d8f0af425f
Francisco Bravo <bravofran3@gmail.com>
2024-07-19 12:54:28 +0200

    changed walker to node way

--- utils/walker.ts
+++ utils/walker.ts
@@ -1,12 +1,12 @@
-import { walk } from "https://deno.land/std/fs/mod.ts";
-import { serviceProcessor } from "../src/serviceProcessor.ts";
+import { serviceProcessor } from "../src/serviceProcessor";
+import { opendirSync, readFileSync } from "fs";
 
 export async function walker() {
-  for await (const dirEntry of walk("../emailConfigurations", {
-    exts: [".json"],
-  })) {
+  for await (
+    const dirEntry of await opendirSync("./emailConfigurations")
+  ) {
     //TODO change to look for folders instead of files
-    const jsonConfigFile = await Deno.readFile(dirEntry.path);
+    const jsonConfigFile = await readFileSync(dirEntry.path);
     const jsonConfigText = new TextDecoder().decode(jsonConfigFile);
     const jsonConfig = JSON.parse(jsonConfigText);
 
