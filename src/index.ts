982fa06a98a12f4704a698d73b2192356219ad3f
Francisco Bravo <bravofran3@gmail.com>
2024-07-20 12:23:30 +0200

    fniish scripting the logic

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-import { walker } from "../utils/walker";
+import { serviceProcessor } from "../src/serviceProcessor";
+import { opendirSync, readFileSync } from "fs";
 
+export async function walker() {
+    for await (
+        const dirEntry of await opendirSync("./emailConfigurations")
+    ) {
+        //TODO change to look for folders instead of files1
+        const jsonConfigFile = await readFileSync(dirEntry.path);
+        const jsonConfigText = new TextDecoder().decode(jsonConfigFile);
+        const jsonConfig = JSON.parse(jsonConfigText);
+
+        serviceProcessor(jsonConfig);
+    }
+}
 walker();
